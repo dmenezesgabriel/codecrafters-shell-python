4d4d799e615fe9e4f4bcb846a5b19d18289255dc
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2026-07-04 14:34:14 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,9 +10,8 @@ def main():
             break
         if command.startswith("echo"):
             print(command[5:])
-        if command.startswith("type"):
-            if command[5:] in ["exit", "echo", "type"]:
-                print(f"{command[5:]} is a shell builtin")
+        if command.startswith("type") and command[5:] in ["exit", "echo", "type"]:
+            print(f"{command[5:]} is a shell builtin")
         else:
             print(f"{command}: not found")
 
